71d76bfbb51c37cc6f602522c2f1d758b6d7449f
kilianpaquier <kilian@kilianpaquier.com>
2024-08-17 07:46:24 +0000

    docs: package renaming reference in doc.go
    Signed-off-by: kilianpaquier <kilian@kilianpaquier.com>

--- pkg/cfs/doc.go
+++ pkg/cfs/doc.go
@@ -1,4 +1,4 @@
 /*
-The fs package exposes some useful function around files. Please check the functions to know what it offers.
+The cfs package exposes some useful function around files. Please check the functions to know what it offers.
 */
 package cfs
--- pkg/clog/doc.go
+++ pkg/clog/doc.go
@@ -1,5 +1,5 @@
 /*
-The logger package is a simple package providing a default interface for logging.
+The clog package is a simple package providing a default interface for logging.
 
 It allows any developer to use any logging implementation. For more information see Logger interface.
 */
